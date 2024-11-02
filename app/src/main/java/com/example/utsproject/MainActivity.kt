f98d271b89f323bff81c1c5f2a1daf5f2cc4e2e0
Falah Mandira Irawan <falah_1313622038@mhs.unj.ac.id>
2024-11-02 11:10:50 +0700

    create floating action button

--- app/src/main/java/com/example/utsproject/MainActivity.kt
+++ app/src/main/java/com/example/utsproject/MainActivity.kt
@@ -6,9 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.utsproject.ui.theme.UTSProjectTheme
@@ -16,17 +23,35 @@ import com.example.utsproject.ui.screen.BookListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             UTSProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    BookListScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CustomScaffold()
+
             }
         }
+    }
+}
+
+@Composable
+fun CustomScaffold() {
+    var presses by remember { mutableIntStateOf(0) }
+//    kalo remember error jangan lupa import androidx.compose.runtime.*
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+
+        floatingActionButton = {
+            FloatingActionButton(onClick = { presses++ }) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }
+    ) { innerPadding ->
+        BookListScreen(
+            modifier = Modifier.padding(innerPadding)
+        )
     }
 }
 
